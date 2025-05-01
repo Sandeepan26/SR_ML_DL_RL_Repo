8a3d16ec3a01507e5d7c98ad0a7f7b7c793d2d4e
Sandeepan26 <sandeepan_26@LAPTOP-USVUF83N.>
2025-05-01 18:08:46 +0000

    Adding updated files

--- neural_networks/README.md
+++ neural_networks/README.md
@@ -1,6 +1,3 @@
-## This directory is currently  work in progress
-
-The codes in this directory are related to implmentation of Neural Networks and various models for computation </br> The codes are commented in order to make it beginner-friendly
 
 The codes can be run on a Jupyter Notebook or on a terminal with the commands
 ```
@@ -12,6 +9,7 @@ To run the files normally on terminal
 ```
 python3 <file>.py
 ```
+In case of using WSL, please make sure to use a Python Virtual Environment
 
 Python version : 3.10 or above </br>
 Pytorch version: 2.7 or above in case of latest release
--- neural_networks/basic_neural_network.py
+++ neural_networks/basic_neural_network.py
@@ -1,12 +1,94 @@
-#This code is a work in progress. Will be pushing the updated code 
 
 #Imports
 
 import torch
-from sklearn import model_selection
 
-#creating a simple model with the help of torch.nn module
 
-toy_model = torch.nn.Linear(4,2)  #here number of input features = 4 and number of output features = 2
+"""
+    @create_model is a class meant for creating a simple 6-layer neural network . It can also return a single layer based on the function call
 
-toy_model(torch.randn(4,4))  #passing random inputs to the model
+    functions: 
+    init: takes two arguments which are used for creating single neuron 
+    
+    show_model: prints the model tensor along with the parameters
+    
+    sdg: a simple sigmoid gradient descent implementation. torch.optim.SGD can also be used in place for computing that
+    
+    torch.optim.SGD(model.paramters(), lr = 1e-2, momentum = 0.8)  
+        where lr is learning rate, and momentum is for the pace at which the optimizer is supposed to calcualte the minima
+
+    generate_neural_network: generates a 6-layer neural network with same padding, stride and kernel size
+
+"""
+class create_model:
+
+    def __init__(self, in_features: int = 1, out_features: int = 1):
+        self.toy_model = torch.nn.Linear(in_features, out_features)
+
+    def show_model(self):
+        print("Here is your model:", self.toy_model)        
+
+    '''
+    Simple function to computer sigmoid gradient descent. It can also be done via calling SGD from torch.optim 
+    This funtion works in the following steps:
+    1. initialize a tensor of batch size equal to the out_features and dimensions of the in_features of the toy model
+    2. The loop iterates through the limit and performs the following operations:
+        - passes the model xb tensor
+        - calculates mean squared error
+        - appends to a list
+        - zero_grad() methos to set the gradients of the model as zeroes before backpropagation as backprop adds the gradients with the previous gradients
+        - computes the SGD by subtracting the parameters with grad * learning rate
+
+    '''
+    def sgd(self, limit: int = 1000):
+        self.log = []
+        self.ref_tensor = torch.tensor([[1.0, 1.0]])
+        self.xb = torch.randn(self.toy_model.out_features, self.toy_model.in_features)
+        for x in range(limit):
+            self.y_b = self.toy_model(self.xb)
+            self.loss = ((self.y_b - self.ref_tensor)**2).sum(1).mean()
+            self.log.append(self.loss.item())
+            self.toy_model.zero_grad()   #initialize gradients to zero before backpropagation
+            self.loss.backward()
+            with torch.no_grad():
+                for prm in self.toy_model.parameters():
+                    prm[...] -= 0.01 * prm.grad  
+        return prm
+
+    '''
+        Generates a Neural Network with ReLU (Rectified Linear Unit) as activation function and a 2D convolution layer with the provided arguments
+    '''
+    def generate_neural_network(self):
+        self.neural_network = torch.nn.Sequential(torch.nn.Conv2d(1, 4, kernel_size=2, padding= 2, stride=2), 
+                                     torch.nn.ReLU(),
+                                     torch.nn.Conv2d(in_channels=4, out_channels=8, kernel_size=2, padding= 2, stride=2), 
+                                     torch.nn.ReLU(),
+                                     torch.nn.Conv2d(in_channels=8, out_channels=16, kernel_size=2, padding= 2, stride=2), 
+                                     torch.nn.ReLU(),
+                                     torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2, padding= 2, stride=2), 
+                                     torch.nn.ReLU(),
+                                     torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, padding= 2, stride=2), 
+                                     torch.nn.ReLU(),
+                                     torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=2, padding= 2, stride=2), 
+                                     torch.nn.ReLU(),
+                                     torch.nn.AdaptiveAvgPool2d(1),
+                                     torch.nn.Flatten(),
+                                     torch.nn.Linear(64,8))
+        return self.neural_network
+
+    def display_neural_network(self):
+        print ("Neural Network\n", self.neural_network)
+
+
+
+if(__name__ == "__main__"):
+
+    model = create_model(3,2)
+
+    model.show_model()
+
+    print("Tensor after SGD",model.sgd())
+
+    model.generate_neural_network()
+
+    model.display_neural_network()
